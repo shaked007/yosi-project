8c6f83a446a09e41cefa072580288868ccfebc44
shaked007 <shakedbukai@gmail.com>
2023-02-04 20:46:32 +0200

    plus router

--- src/views/PrintView.vue
+++ src/views/PrintView.vue
@@ -1,4 +1,40 @@
 <template>
+
+<dialog  ref="dialog">  
+    <h6> דוחות</h6> 
+ <h5 >{{modalText}} </h5>
+  <h5> בתאריכים:  {{dates}}</h5> 
+  <img :src="require('@/assets/report.jpg') " >
+<div class="action-btn-flex">
+    <button @click="printHandler" class="print-btn"> 
+         <v-icon> 
+                mdi mdi-printer
+        </v-icon>
+        הדפס
+    </button>
+    <download-excel 
+    worksheet="אוסף דוחות"
+    :data="filteredDrives"
+    :fields="fields"
+    name="אוסף דוחות.xls"
+    :header="[modalText, dates]"
+    class="excel-btn">
+    <v-icon> mdi mdi-microsoft-excel</v-icon>
+        ייצא לאקסל
+    </download-excel>
+
+    <button @click="backHandler" class="return-btn"> 
+        <v-icon> 
+                mdi mdi-window-close
+        </v-icon>
+        חזור
+    </button>
+     </div>
+
+ </dialog>
+
+
+
   <h4 class="main-title"> טופס שילוח משימה</h4>
  <h6 class="sub-title"> הדפסת דוחות</h6>
 <div class="password-container" v-if="!isAuthenticated">
@@ -77,7 +113,47 @@ import moment from "moment"
 import axios from "axios"
 export default {
     data(){
-        return{ 
+        return{
+            fields:{
+                "תאריך":'date',
+                "שם מפקד משלח משימה":'mefaked-meshaleh-mesima',
+                'מ"א מפקד משלח משימה':'mefaked-meshaleh-mesima-num',
+                "שם מפקד משימה":'mefaked-mesima',
+                'מ"א מפקד משימה':'mefaked-mesima-num',
+                "שם נהג":'driver',
+                'מ"א נהג':'driver-num',
+                'שעת תדרוך':'tidruh-time',
+                'נוסע 1':'nosea1',
+                'נוסע 1 מ"א':'nosea1-num',
+                'נוסע 2':'nosea2',
+                'נוסע 2 מ"א':'nosea2-num',
+                'נוסע 3':'nosea3',
+                'נוסע 3 מ"א':'nosea3-num',
+                'נוסע 4':'nosea4',
+                'נוסע 4 מ"א':'nosea4-num',
+                 'נוסע 5':'nosea5',
+                'נוסע 5 מ"א':'nosea5-num',
+                'תיאור':'description',
+                'רכב':'car',
+                'האם ישן שמונה שעות לפני נסיעה':'is-sleep',
+                'האם יש רישיון':'is-licensed',
+                'האם נהג בדרך זו בעבר':'is-drove-before',
+                'יעד':'destination',
+                'סכנות':'dangers',
+                'עצירה 1':'stop1',
+                'עצירה 2':'stop2',
+                'עצירה 3':'stop3',
+                'איש קשר 1':'contact1',
+                'טלפון איש קשר 1':'contact1-num',
+                'האם תודרך':'is-tudrah',
+                'האם כרטיס נהיגה בתוקף':'is-card' 
+
+
+                
+            },
+            filteredDrives:[],
+            modalText:'', 
+            dates:'',
             driverModel:"",
             filterType:"",
             dateModel:{from:"",to:""},
@@ -98,6 +174,19 @@ export default {
         }
     },
 methods:{
+    printHandler(){
+        this.$refs.dialog.close()
+     setTimeout(()=>{
+                        sessionStorage.setItem('reports',JSON.stringify(this.filteredDrives))
+                        this.$router.push("/print-final-view")
+                },1000)
+    },
+    backHandler(){
+        this.$refs.dialog.close()
+        this.filteredDrives = [] 
+         this.carValue = ''
+        this.dateModel =  {from:"",to:""}
+    },
    dateCheck(from,to,check) {
 
     var fDate,lDate,cDate;
@@ -114,69 +203,32 @@ methods:{
 
      async filterAndNavigate(){
         if(this.filterType == 'car'){
-        const item = this.handleRangeAndCarFilter(this.dateModel)
-         let dates ;
+        this.filteredDrives = this.handleRangeAndCarFilter(this.dateModel)
               if(this.dateModel.from  && this.dateModel.to){
-                    dates = `${this.dateModel.to.split('/')[2]}/${this.dateModel.to.split('/')[1]}/${this.dateModel.to.split('/')[0]} - ${this.dateModel.from.split('/')[2]}/${this.dateModel.from.split('/')[1]}/${this.dateModel.from.split('/')[0]} `
+                    this.dates = `${this.dateModel.to.split('/')[2]}/${this.dateModel.to.split('/')[1]}/${this.dateModel.to.split('/')[0]} - ${this.dateModel.from.split('/')[2]}/${this.dateModel.from.split('/')[1]}/${this.dateModel.from.split('/')[0]} `
               }else{
-                    dates= 'כל התאריכים'
+                    this.dates = 'כל התאריכים'
               }
+               this.modalText   =  ` נמצאו ${this.filteredDrives.length} דוחות 
+                ברכב ${this.carValue}`
+
+            this.$refs.dialog.show()
+          this.$refs.dialog.classList.add("ani-class")
               
-         const response = await this.$swal({
-                icon:"info",
-                title:'כמות הדוחות',
-                text:`  נמצאו ${item.length} דוחות 
-                עם הרכב ${this.carValue}
-                בין התאריכים : 
-                  ${dates}` 
-                 ,  
-                 confirmButtonText:'הדפס',
-                 showCancelButton:true,
-                 cancelButtonText:'חזור'
-                 })
-                if (response.isConfirmed){
-                             this.$swal.close()
-                        this.$swal.closeModal()
-                        setTimeout(()=>{
-                        sessionStorage.setItem('reports',JSON.stringify(item))
-                        this.$router.push("/print-final-view")
-                },1000)
-            }else{
-                this.carValue = ''
-                this.dateModel =  {from:"",to:""}
-            }
         }else if (this.filterType =='driver'){
-            const item = this.handleRangeAndDriverFilter(this.dateModel)
-            let dates ;
+            this.filteredDrives = this.handleRangeAndDriverFilter(this.dateModel)
+          
               if(this.dateModel.from  && this.dateModel.to){
-                    dates = `${this.dateModel.to.split('/')[2]}/${this.dateModel.to.split('/')[1]}/${this.dateModel.to.split('/')[0]} - ${this.dateModel.from.split('/')[2]}/${this.dateModel.from.split('/')[1]}/${this.dateModel.from.split('/')[0]} `
+                    this.dates  = `${this.dateModel.to.split('/')[2]}/${this.dateModel.to.split('/')[1]}/${this.dateModel.to.split('/')[0]} - ${this.dateModel.from.split('/')[2]}/${this.dateModel.from.split('/')[1]}/${this.dateModel.from.split('/')[0]} `
               }else{
-                    dates= 'כל התאריכים'
+                    this.dates = 'כל התאריכים'
               }
-           const response = await this.$swal({
-                icon:"info",
-                title:'כמות הדוחות',
-                text:` נמצאו ${item.length} דוחות 
-                של הנהג ${this.driverModel}
-                 בין התאריכים : 
-                ${dates}` 
-              ,  
-                   confirmButtonText:'הדפס',
-                 showCancelButton:true,
-                 cancelButtonText:'חזור'
-            })
-            if (response.isConfirmed){
-                this.$swal.close()
-                this.$swal.closeModal()
-                setTimeout(()=>{
-                    sessionStorage.setItem('reports',JSON.stringify(item))
-                    this.$router.push("/print-final-view")
-                },1000)
-        
-            }else{
-                this.driverModel = ''
-                this.dateModel =  {from:"",to:""}
-            }
+            this.modalText =` נמצאו ${this.filteredDrives.length} דוחות 
+                 של הנהג ${this.driverModel}`
+          
+                 this.$refs.dialog.show()
+          this.$refs.dialog.classList.add("ani-class")
+       
    
         }
     },
@@ -259,6 +311,75 @@ methods:{
 </script>
 
 <style scoped>
+.excel-btn{
+    cursor: pointer;
+          background-color: green;
+    color: white;
+    padding: 0.5em 1em;
+    border-radius: 20px;
+}
+.print-btn{
+      background-color: rgb(13,110,253);
+    color: white;
+    padding: 0.5em 1em;
+    border-radius: 20px;
+}
+.return-btn{
+    background-color: red;
+    color: white;
+    padding: 0.5em 1em;
+    border-radius: 20px;
+}
+.action-btn-flex{
+    display: flex;
+    flex-direction: row;
+    width: 100px;
+    width: 80%;
+    margin: 0 auto;
+    justify-content: space-evenly;
+}
+.ani-class{
+  animation: zoom 0.3s ease-in-out forwards;
+  
+}
+@keyframes zoom{
+  0%{
+    opacity: 0;
+    transform:translate(-50%,-50%) scale(0.8) ;
+  }
+  100%{
+    opacity: 1;
+    transform: translate(-50%,-50%)  scale(1);
+  }
+  
+}
+dialog img{
+    width: 90%;
+    height: 65%;
+}
+dialog h5{
+  text-align: center;
+  color: grey;
+  font-size: 1rem;
+}
+dialog{
+  opacity: 0;
+  text-align: center;
+  width: 500px;
+  position: fixed;
+  top: 50%;
+  z-index: 100;
+  height: 50%;
+  border: none;
+  border-radius: 20px;
+  left: 50%;
+  transform: translate(-50%,-50%) scale(0.8);
+}
+dialog::backdrop {
+  background: rgba(0,0,0,1);
+}
+
+
 .drivers-select-container{
     margin-bottom: 10px;
     direction: rtl !important;
